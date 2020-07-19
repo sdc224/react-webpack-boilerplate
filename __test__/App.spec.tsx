fe196b1673085d9f9b34d567bc033f2097498844
Sourodeep Chatterjee <sourodeep224@outlook.com>
2020-07-19 15:42:12 +0530

    Moved tests outside of src, reducing bundle

--- __test__/App.spec.tsx
+++ __test__/App.spec.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, RenderResult } from "@testing-library/react";
-import App from "../App";
+import App from "../src/App";
 
 let documentBody: RenderResult;
 
--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	preset: "ts-jest",
 	testEnvironment: "jsdom",
-	setupFilesAfterEnv: ["<rootDir>/src/__test__/config/importJestDOM.ts"]
+	setupFilesAfterEnv: ["<rootDir>/__test__/config/importJestDOM.ts"]
 };
